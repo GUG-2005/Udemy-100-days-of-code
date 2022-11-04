6222eda36d38ec45a6f67fa2227a402247b69006
GUG-2005 <111267744+GUG-2005@users.noreply.github.com>
2022-11-04 07:44:22 -0700

    This is the first day project of 100days_of_code
    This is the first day of my python project even this is my first day of code in my entire life! By, uploading this project on Github makes me fell proud!!

---
+++ band-name-generator.py
@@ -0,0 +1,13 @@
+# Start with a greeting
+print("Welcome Band Name Generator")
+
+#Ask the name of the living city
+city_name = input("What is the name of your city\n")
+
+#Ask the name of the owned pet
+pet_name = input("What is the name of your pet\n")
+
+#Combine the city name and the pet name
+print("Your band name should be: " + city_name + " "+ pet_name)
+
+#Be aware of moving the cursor to the next lime
